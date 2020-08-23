fef1b240ee622836b5aa4e8f6d83daf9a46ea66f
Pavel Durov <pav3ldurov@gmail.com>
2020-08-23 13:26:22 +0100

    chore: pre commit hook + tests

--- test/hidden-files.spec.ts
+++ test/hidden-files.spec.ts
@@ -1,16 +1,14 @@
 import test from "ava";
-
-import { execSync } from "child_process";
 import { assertExec } from "./utils";
-import { PROC_EXIST_ERROR_CODE } from "../src/const";
+import { PROC_EXIST_ERROR_CODE as ERROR } from "../src/const";
 
 test("expected to find pattern in hidden file - raw string", (t) => {
   const cmd = "yarn start test/file-samples/l1-d/l2-d/.*.txt";
   const pattern = "apikey-1234567890";
   const assert = (str: string) =>
     /SECRET=apikey-1234567890/.test(str) && /l2-f3-hidden.txt/.test(str);
-  assertExec(t, `${cmd} --pattern '${pattern}'`, assert, PROC_EXIST_ERROR_CODE);
-  assertExec(t, `${cmd} -p '${pattern}'`, assert, PROC_EXIST_ERROR_CODE);
+  assertExec(t, `${cmd} --pattern '${pattern}'`, assert, ERROR);
+  assertExec(t, `${cmd} -p '${pattern}'`, assert, ERROR);
 });
 
 test("expected to find pattern in hidden file - regex #1", (t) => {
@@ -18,8 +16,8 @@ test("expected to find pattern in hidden file - regex #1", (t) => {
   const pattern = "[a-z]{6}-\\d{10}";
   const assert = (str: string) =>
     /SECRET=apikey-1234567890/.test(str) && /l2-f3-hidden.txt/.test(str);
-  assertExec(t, `${cmd} --pattern '${pattern}'`, assert, PROC_EXIST_ERROR_CODE);
-  assertExec(t, `${cmd} -p '${pattern}'`, assert, PROC_EXIST_ERROR_CODE);
+  assertExec(t, `${cmd} --pattern '${pattern}'`, assert, ERROR);
+  assertExec(t, `${cmd} -p '${pattern}'`, assert, ERROR);
 });
 
 test("expected to find pattern in hidden file - regex #2", (t) => {
@@ -28,6 +26,6 @@ test("expected to find pattern in hidden file - regex #2", (t) => {
   const pattern = "apikey-\\d{10}";
   const assert = (str: string) =>
     /SECRET=apikey-1234567890/.test(str) && /l2-f3-hidden.txt/.test(str);
-  assertExec(t, `${cmd} --pattern '${pattern}'`, assert, PROC_EXIST_ERROR_CODE);
-  assertExec(t, `${cmd} -p '${pattern}'`, assert, PROC_EXIST_ERROR_CODE);
+  assertExec(t, `${cmd} --pattern '${pattern}'`, assert, ERROR);
+  assertExec(t, `${cmd} -p '${pattern}'`, assert, ERROR);
 });
